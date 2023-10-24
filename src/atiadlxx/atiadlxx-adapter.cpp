1d5d5a137f9ee0b8eb5acbf73b92195a1045d6a1
Etaash Mathamsetty <etaash.mathamsetty@gmail.com>
2023-10-24 18:37:02 -0400

    More ADL2 functions

--- src/atiadlxx/atiadlxx-adapter.cpp
+++ src/atiadlxx/atiadlxx-adapter.cpp
@@ -96,4 +96,75 @@ extern "C"
 
         return ADL2_Adapter_AdapterInfo_Get(context, *info, sizeof(AdapterInfo) * num_adapters);
     }
+
+    int __stdcall ADL2_Adapter_MemoryInfo_Get(ADL_CONTEXT_HANDLE context, int index, ADLMemoryInfo* info)
+    {
+        ADL_CONTEXT* adl_context = (ADL_CONTEXT*)context;
+        ADL_LOCK();
+
+        if (!info)
+            return ADL_ERR_INVALID_PARAM;
+
+        PFN_vkEnumeratePhysicalDevices func = (PFN_vkEnumeratePhysicalDevices)adl_context->vk_get_instance_proc_addr(
+                                                                                adl_context->vk_instance, "vkEnumeratePhysicalDevices");
+        PFN_vkGetPhysicalDeviceMemoryProperties func2 = (PFN_vkGetPhysicalDeviceMemoryProperties)adl_context->vk_get_instance_proc_addr(
+                                                                                adl_context->vk_instance, "vkGetPhysicalDeviceMemoryProperties");
+
+
+        if (!func || !func2)
+        {
+            printf("ERROR: ADL2_Adapter_MemoryInfo_Get failed to get vulkan funcs\n");
+            return ADL_ERR;
+        }
+
+        uint32_t num_adapters_vk = 0;
+        func(adl_context->vk_instance, &num_adapters_vk, nullptr);
+
+        VkPhysicalDevice* adapters = (VkPhysicalDevice*)calloc(num_adapters_vk, sizeof(VkPhysicalDevice));
+        if (!adapters)
+        {
+            printf("ERROR: ADL2_Adapter_MemoryInfo_Get calloc failed\n");
+            return ADL_ERR;
+        }
+
+        func(adl_context->vk_instance, &num_adapters_vk, adapters);
+
+        VkPhysicalDeviceMemoryProperties props;
+
+        func2(adapters[index], &props);
+
+        memset(info, 0, sizeof(ADLMemoryInfo));
+
+        for(uint32_t i = 0; i < props.memoryHeapCount; i++)
+        {
+            if(props.memoryHeaps[i].flags & VK_MEMORY_HEAP_DEVICE_LOCAL_BIT)
+                info->iMemorySize += props.memoryHeaps[i].size;
+        }
+
+        //random number for now
+        info->iMemoryBandwidth = 256000;
+
+        free(adapters);
+        return ADL_OK;
+    }
+
+    int __stdcall ADL2_Adapter_Graphic_Core_Info_Get(ADL_CONTEXT_HANDLE context, int index, ADLGraphicCoreInfo* info)
+    {
+
+        if(!info)
+        {
+            printf("ERROR: ADL2_Adapter_Graphic_Core_Info_Get invalid param\n");
+            return ADL_ERR_INVALID_PARAM;
+        }
+
+        printf("FIXME: ADL2_Adapter_Graphic_Core_Info_Get stub!\n");
+
+        //dummy values for now (rx 6800xt)
+        memset(info, 0, sizeof(ADLGraphicCoreInfo));
+        info->iGCGen = ADL_GRAPHIC_CORE_GENERATION_RDNA;
+        info->iNumCUs = 72;
+        info->iNumROPs = 128;
+
+        return ADL_OK;
+    }
 }
